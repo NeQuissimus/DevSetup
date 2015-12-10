41416406e55fa2b23f3aee92bb66b1dcca5397c5
Tim Steinbach <steinbach.tim@googlemail.com>
2015-12-10 17:56:08 +0000

    Update laptop config

--- configuration.nix
+++ configuration.nix
@@ -1,24 +1,57 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ /etc/nixos/hardware-configuration.nix ];
+  imports = [ /etc/nixos/hardware-configuration.nix ./ohmyzsh.nix ./xterm.nix ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    cleanTmpDir = true;
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/sda";
+    kernel.sysctl = {
+      "vm.swappiness" = 10;
+    };
 
-  swapDevices = [ { device = "/dev/sda1"; } ];
+    kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 10;
+    loader.grub = {
+      enable = true;
+      version = 2;
+      device= "/dev/sda";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    binutils
+    chromium
+    dropbox
+    gitFull
+    gradle
+    htop
+    jre
+    maven
+    sublime3
+    texLiveFull
+  ];
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      source-code-pro
+    ];
+  };
+
+  i18n = {
+    consoleFont = "source-code-pro";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
   };
 
   networking = {
-    hostName = "nixPixel";
-    extraHosts = "127.0.0.1 nixPixel";
-    hostId = "6377bdf4";
+    hostName = "nixel";
+    extraHosts = ''
+      127.0.0.1 nixel
+      127.0.0.1 ftp.au.debian.org
+    ''; # Basically kill ftp.au.debian.org
 
     firewall = {
       enable = true;
@@ -27,6 +60,9 @@
   };
 
   nix = {
+    binaryCaches = [ http://cache.nixos.org http://hydra.nixos.org ];
+    binaryCachePublicKeys = [ "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs=" ];
+
     gc = {
       automatic = true;
       dates = "10:00";
@@ -42,29 +78,6 @@
     useChroot = true;
   };
 
-  powerManagement = {
-    enable = true;
-    cpuFreqGovernor = "ondemand";
-  };
-  
-  i18n = {
-    consoleFont = "source-code-pro";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  time = {
-    timeZone = "UTC";
-  };
-
-  fonts = {
-    enableFontDir = true;
-    enableGhostscriptFonts = true;
-    fonts = with pkgs; [
-      source-code-pro
-    ];
-  };
-
   nixpkgs.config = {
     allowUnfree = true;
     chromium = {
@@ -73,22 +86,10 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    binutils
-    chromium
-    dropbox
-    ghc
-    gitFull
-    gnumake
-    htop
-    maven
-    jdk
-    sbt
-    scala
-    stack
-    sublime3
-    texLiveFull
-  ];
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "ondemand";
+  };
 
   programs.zsh = {
     enable = true;
@@ -115,21 +116,30 @@
       desktopManager.xterm.enable = false;
       displayManager.slim.enable = true;
     };
+
+    nscd.enable = false;
   };
 
-  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
+  time = {
+    timeZone = "UTC";
+  };
 
-  users.extraUsers.nequi = {
-   name = "nequi";
-   group = "users";
-   uid = 1000;
-   extraGroups = [ "wheel" ];
-   createHome = true;
-   home = "/home/nequi";
-   shell = "/run/current-system/sw/bin/zsh";
-   openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3W3w6Zf5ceFx+7FFfLKZZb2eeHf6fFETSfbBw954aiFQkVdo0MBgxICc8HzjSk+XKsrgYB/uDFs5e+qjyqmS2nKJMiMTkTRxG+HOiPYf50LI5eZPqB5mMORDOQZYShAYfMs5+dLfDT2K88zuAb4KoxoJVo2a6Q1x+NxJT6YKtcTgxmAGylqRJThvpVd/yoYoBKzlmWPI37iyeg+8GT5Qo9edzqYFIg4tN9lK1mzpZ+ybpL674+WM6X9bGdlKNn0JHi9BCsFUGYFvB9qpQuEZ8MRL/cvfDrERZuc+w6WaCcZobAd0s6GTylUjGGTSvrcXYxPexFMPZIgNe0qRjacfd tsteinbach@pixelbox.local"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFnJWxz0uwcdMkyjx3+MxO9ZNlVPaCGsUolCi9GtRDJ4 nequi@nixPixel"
-   ];
+  users = {
+    defaultUserShell = "/run/current-system/sw/bin/zsh";
+
+    extraUsers.nequi = {
+     createHome = true;
+     extraGroups = [ "wheel" ];
+     group = "users";
+     home = "/home/nequi";
+     name = "nequi";
+     shell = "/run/current-system/sw/bin/zsh";
+     uid = 1000;
+
+     openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3W3w6Zf5ceFx+7FFfLKZZb2eeHf6fFETSfbBw954aiFQkVdo0MBgxICc8HzjSk+XKsrgYB/uDFs5e+qjyqmS2nKJMiMTkTRxG+HOiPYf50LI5eZPqB5mMORDOQZYShAYfMs5+dLfDT2K88zuAb4KoxoJVo2a6Q1x+NxJT6YKtcTgxmAGylqRJThvpVd/yoYoBKzlmWPI37iyeg+8GT5Qo9edzqYFIg4tN9lK1mzpZ+ybpL674+WM6X9bGdlKNn0JHi9BCsFUGYFvB9qpQuEZ8MRL/cvfDrERZuc+w6WaCcZobAd0s6GTylUjGGTSvrcXYxPexFMPZIgNe0qRjacfd tsteinbach@pixelbox.local"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFnJWxz0uwcdMkyjx3+MxO9ZNlVPaCGsUolCi9GtRDJ4 nequi@nixPixel"
+     ];
+    };
   };
 }

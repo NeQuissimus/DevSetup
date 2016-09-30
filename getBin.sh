0eae3904a0847932f085428d208e8a7be196aa17
Tim Steinbach <tim@nequissimus.com>
2016-09-30 10:27:00 -0400

    Change bash for httpstats

--- getBin.sh
+++ getBin.sh
@@ -4,5 +4,6 @@ mkdir -p ./bin
 rm -rf ./bin/*
 
 curl https://raw.githubusercontent.com/b4b4r07/httpstat/master/httpstat.sh -o ./bin/httpstat
+sed -i 's/\/bin\/bash/\/usr\/bin\/env bash/g' ./bin/httpstat
 
 chmod +x ./bin/*

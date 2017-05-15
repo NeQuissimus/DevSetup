5431cb3837539962199c797bf6218874453c9c81
Tim Steinbach <tim@nequissimus.com>
2017-05-15 09:42:54 -0400

    Add emacs

---
+++ emacs.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  myEmacs = pkgs.emacs;
+  emacsWithPackages = (pkgs.emacsPackagesNgGen myEmacs).emacsWithPackages;
+in
+  emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
+  ]) ++ (with epkgs.melpaPackages; [
+    ensime
+    use-package
+  ]) ++ (with epkgs.elpaPackages; [
+    beacon
+  ]) ++ [
+    pkgs.notmuch
+  ])

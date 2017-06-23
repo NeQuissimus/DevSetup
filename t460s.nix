e050ec635429bac704a8d83073207dc893336741
Tim Steinbach <tim@nequissimus.com>
2017-06-23 08:45:31 -0400

    Hardened kernel

--- t460s.nix
+++ t460s.nix
@@ -15,7 +15,7 @@
       "vm.swappiness" = 1;
     };
 
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_hardened;
 
     loader = {
       efi.canTouchEfiVariables = true;

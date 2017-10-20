af65c5f5e48422886557fc42a8679ca399515a66
Tim Steinbach <tim@nequissimus.com>
2017-10-20 10:01:51 -0400

    Remove crap from sway

--- nixos-sway.nix
+++ nixos-sway.nix
@@ -1,22 +1,14 @@
 { config, lib, pkgs, ... }: rec {
    config.services.xserver = {
     autorun = true;
-    defaultDepth = 24;
 
     displayManager = {
-      gdm = {
+      sddm = {
         enable = true;
       };
     };
 
     enable = true;
-    exportConfiguration = true;
-
-    synaptics = {
-      enable = true;
-      tapButtons = false;
-      twoFingerScroll = true;
-    };
 
     windowManager.sway = {
       enable = true;

4e716dc3f0c90a2b10a3d2307dd472d0537caaba
Tim Steinbach <tim@nequissimus.com>
2018-05-22 06:55:26 -0400

    nix-shell kubectl

--- users/nequi/default.nix
+++ users/nequi/default.nix
@@ -403,6 +403,7 @@ in mkHome {
       function sbt() { args="$@"; nix-shell -p sbt-extras -p nodejs -p jekyll --command "sbt -J-Xms1G -J-Xmx8G ''${args}" }
       function amm() { nix-shell -p ammonite --command "amm" }
       function travis() { args="$@"; nix-shell -p travis --command "travis ''${args}" }
+      function kubectl() { args="$@"; nix-shell -p kubectl --command "kubectl ''${args}" }
 
       # Include shell completions
       type -p kubectl >> /dev/null && source <(kubectl completion zsh)

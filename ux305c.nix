4dc1826f41fbbec436720c3da1af6a200f1d060b
Tim Steinbach <tim@nequissimus.com>
2018-09-29 21:38:12 -0400

    Switch UX305C to 19.03

--- ux305c.nix
+++ ux305c.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports = [ ./nixos-common.nix ./nixos-common-1809.nix ./nixos-harden.nix ./nixos-xmonad.nix ./ux305c-hardware.nix ./ux305c-wifi.nix ];
+  imports = [ ./nixos-common.nix ./nixos-common-1903.nix ./nixos-harden.nix ./nixos-xmonad.nix ./ux305c-hardware.nix ./ux305c-wifi.nix ];
 
   boot.loader.grub = {
     device = "nodev";

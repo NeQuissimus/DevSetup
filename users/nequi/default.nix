b1da3fa94727ab002ea93d261cd34fd43dc7b3af
Tim Steinbach <tim@nequissimus.com>
2019-01-26 23:08:43 -0500

    Nicer xmobar

--- users/nequi/default.nix
+++ users/nequi/default.nix
@@ -70,17 +70,18 @@ in mkHome {
       import System.IO
       import qualified Data.Map as M
       import qualified XMonad.StackSet as W
-      myWorkspaces = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ]
+      --  = 61728
+      --  = 61729
+      --  = 62057
+      --  = 61848
+      myWorkspaces = [ "\61728", "\61729", "3", "4", "5", "6", "7", "8", "\62057", "\61848" ]
       myManageHook = composeAll [
           isFullscreen --> (doF W.focusDown <+> doFullFloat)
           , isDialog --> doFloat
-          , className =? "Chromium-browser" --> viewShift "8"
-          , className =? "Firefox" --> viewShift "9"
-          , className =? "Emacs" --> viewShift "2"
-          , className =? "Atom" --> viewShift "2"
-          , className =? "Code" --> viewShift "2"
-          , className =? "HipChat" --> viewShift "0"
-          , className =? "Slack" --> viewShift "0"
+          , className =? "Chromium-browser" --> viewShift "\62057"
+          , className =? "Firefox" --> viewShift "\62057"
+          , className =? "Emacs" --> viewShift "\61729"
+          , className =? "Slack" --> viewShift "\61848"
           , appName =? "desktop_window" --> doIgnore
           ]
           where viewShift = doF . liftM2 (.) W.greedyView W.shift
@@ -100,6 +101,13 @@ in mkHome {
         ++ [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
            | (key, sc) <- zip [xK_q, xK_w] [0, 1]
            , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+      myPP = xmobarPP {
+          ppOutput          = putStrLn
+        , ppCurrent         = xmobarColor "#EEEE00" "" . wrap " " " "
+        , ppTitle           = xmobarColor "yellow"  "" . shorten 20
+        , ppLayout          = shorten 6
+        , ppUrgent          = xmobarColor "red" "yellow"
+        }
       main = do
         xmproc <- spawnPipe "xmobar /home/${user}/.xmobar/config"
         xmonad $ def {
@@ -107,7 +115,7 @@ in mkHome {
           handleEventHook = docksEventHook <+> handleEventHook def,
           keys = myKeys,
           layoutHook = avoidStruts $ layoutHook def,
-          logHook = dynamicLogWithPP xmobarPP
+          logHook = dynamicLogWithPP myPP
                           { ppOrder = \(ws:l:t:_) -> ws : [t]
                           , ppOutput = hPutStrLn xmproc
                           , ppTitle = xmobarColor "green" "" . shorten 50 },

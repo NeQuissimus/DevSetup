74e8fb6f2f1b8ddeadf66d44fd5acb35b8bb8f50
Tim Steinbach <tim@nequissimus.com>
2019-06-08 19:12:01 -0400

    emacs: Scala Metals

--- users/nequi/init.el
+++ users/nequi/init.el
@@ -1,10 +1,12 @@
 (package-initialize)
 
 (require 'smartparens-config)
+(require 'yasnippet)
 
 (beacon-mode 1)
 (global-auto-revert-mode t)
 (global-flycheck-mode)
+(yas-global-mode 1)
 (global-whitespace-mode)
 (load-theme 'spacemacs-dark t)
 (projectile-global-mode)
@@ -139,9 +141,19 @@
 
 ;; Enable scala-mode and sbt-mode
 (use-package scala-mode
-  :mode "\\.s\\(cala\\|bt\\)$")
+  :mode "\\.s\\(cala\\|bt\\)$"
+  :hook (scala-mode . lsp)
+  :custom
+  (scala-indent:align-forms t)
+  (scala-indent:align-parameters t)
+  (scala-indent:indent-value-expression t)
+  (scala-indent:default-run-on-strategy)
+  (scala-indent:operator-strategy)
+  :config
+  (setq lsp-metals-server-command "~/bin/metals-emacs"))
 
 (use-package sbt-mode
+  :after scala-mode
   :commands sbt-start sbt-command
   :config
   ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
@@ -151,15 +163,15 @@
    'self-insert-command
    minibuffer-local-completion-map))
 
+(use-package company-lsp)
+
 ;; Enable nice rendering of diagnostics like compile errors.
 (use-package flycheck
   :init (global-flycheck-mode))
 
-(use-package lsp-mode)
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook (scala-mode . lsp)
+  :config (setq lsp-prefer-flymake nil))
 
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode))
-
-(use-package lsp-scala
-  :load-path "~/dev/lsp-scala"
-  :hook (scala-mode . lsp-scala-enable))
+(use-package lsp-ui)

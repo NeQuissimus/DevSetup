e57499411dcea2e739052158a2d0867626e3d9dc
Tim Steinbach <tim@nequissimus.com>
2019-07-09 13:42:33 -0400

    Extract hosts

--- p320.nix
+++ p320.nix
@@ -1,7 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let
-  ad=lib.fileContents ./etc/ad;
+  ad = lib.fileContents ./etc/ad;
+  esentire-dns = import ./esentire-dns.nix;
 in {
   imports = [ ./nixos-common.nix ./nixos-harden.nix ./nixos-xmonad.nix ./p320-hardware.nix ];
 
@@ -29,36 +30,8 @@ in {
 
   networking.hosts = {
     "127.0.0.1" = ["${config.networking.hostName}" "localhost"];
-    "10.1.108.118" = ["github.internal" "github.esentire.com"];
-    "10.1.108.152" = ["jen001p"];
-    "10.1.108.153" = ["jen002p"];
-    "10.1.108.154" = ["jen003" "registry.internal" "portus-1.internal"];
-    "10.1.108.160" = ["jen004p"];
-    "10.1.110.54" = ["artifactory.internal"];
-    "10.1.110.95" = ["tickets.internal"];
-    "10.1.110.113" = ["gems.internal"];
-    "10.1.110.130" = ["build-01.internal"];
-    "10.1.110.136" = ["kaf001cmb01p.internal"];
-    "10.1.110.137" = ["kaf002cmb01p.internal"];
-    "10.1.110.138" = ["kaf003cmb01p.internal"];
-    "10.1.110.167" = ["jira.esentire.com" "jira.internal"];
-    "10.1.110.178" = ["secnet.esentire.com"];
-    "10.1.110.191" = ["hipchat.esentire.com"];
-    "10.1.110.208" = ["jenkins.internal"];
-    "10.1.110.243" = ["gerrit.internal"];
-    "10.1.110.83" = ["confluence.internal" "confluence.esentire.com"];
-    "10.1.114.20" = ["exchange.esentire.com"];
-    "10.1.115.220" = ["kubeapi.internal"];
-    "10.1.204.60" = ["kubeapi.uat"];
-    "10.1.204.234" = ["soc.uat"];
-    "10.203.0.31" = ["kubeapi.qa"];
-    "10.203.1.101" = ["kubeapi.dev" "kubeapi.test"];
-    #"10.203.1.106" = ["kubeapi.dev"];
-    #"10.203.1.107" = ["kubeapi.dev"];
-    "172.16.0.254" = ["captiveportal-login.esentire.com" "wifi.esentire.com"];
-
     "0.0.0.0" = [ "ftp.au.debian.org" ];
-  };
+  } // esentire-dns.hosts;
 
   networking.networkmanager = {
     enable = true;
--- ux305c.nix
+++ ux305c.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  esentire-dns = import ./esentire-dns.nix;
+in {
   imports = [ ./nixos-common.nix ./nixos-harden.nix ./nixos-xmonad.nix ./ux305c-hardware.nix ./ux305c-wifi.nix ];
 
   boot = {
@@ -24,7 +26,7 @@
     "10.0.10.10" = ["serenitybysarah.ca"];
     "172.16.0.254" = ["wifi.esentire.com"];
     "0.0.0.0" = ["ftp.au.debian.org"];
-  };
+  } // esentire-dns.hosts;
 
   programs.ssh.extraConfig = ''
     Host pine

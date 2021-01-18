be090055f670f6463bee409244a5cb083400410c
Tim Steinbach <tim@nequissimus.com>
2021-01-18 09:31:42 -0500

    Update nix-update

--- nixpkgs/nix-update.nix
+++ nixpkgs/nix-update.nix
@@ -6,13 +6,13 @@ with pkgs.python3Packages;
 
 buildPythonApplication rec {
   pname = "nix-update";
-  version = "0.3";
+  version = "0.3.2";
 
   src = fetchFromGitHub {
     owner = "NeQuissimus";
     repo = pname;
-    rev = "0.3";
-    sha256 = "sha256-cMllWFPK6pwqrocjkZKjnELIdtW4tj5Yu6AMw7Zd2JU=";
+    rev = "0.3.2";
+    sha256 = "1ykxr0yah7zl06igm7wiji9zx3y0xpjc37hbfhn6gnir6ssa0kqp";
   };
 
   makeWrapperArgs = [

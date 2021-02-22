79db4cf0f110f6bfb4ecce43d69e3bff9caad4df
Tim Steinbach <tim@nequissimus.com>
2021-02-22 18:20:55 -0500

    Need iwlwifi module

--- ux305c-hardware.nix
+++ ux305c-hardware.nix
@@ -6,7 +6,7 @@
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-intel" "iwlwifi" ];
   boot.extraModulePackages = [ ];
 
   boot.initrd.postDeviceCommands = lib.mkAfter ''

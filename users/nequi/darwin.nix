90a397d4e5e4ef71676a8119d18f1e7264d4064c
Tim Steinbach <tim.steinbach@shopify.com>
2022-04-05 10:53:20 -0400

    Add Cachix

--- users/nequi/darwin.nix
+++ users/nequi/darwin.nix
@@ -4,6 +4,17 @@
     ./home/emacs.nix
     ./home/git.nix
     ./home/zsh.nix
+
+      (
+        let
+          declCachix = builtins.fetchTarball "https://github.com/jonascarpay/declarative-cachix/archive/a2aead56e21e81e3eda1dc58ac2d5e1dc4bf05d7.tar.gz";
+        in import "${declCachix}/home-manager.nix"
+      )
+  ];
+
+  caches.cachix = [
+    "nequissimus"
+    "nix-community"
   ];
 
   home = {

f175951a44c93621f7b692f000469625a8caba79
Tim Steinbach <tim.steinbach@shopify.com>
2024-12-25 22:23:20 -0500

    Sync

--- supermicro.nix
+++ supermicro.nix
@@ -77,7 +77,7 @@
       systemCronJobs = [
         "0 17 * * 5 root reboot"
         ''
-          0 18 * * 5 root ${pkgs.google-cloud-sdk}/bin/gcloud storage rsync "/tank/immich_enc" "gs://nequi-nas-i/" --recursive --delete-unmatched-destination-objects''
+          0 18 * * 5 root GOOGLE_APPLICATION_CREDENTIALS=/etc/gcs/serviceaccount.json ${pkgs.google-cloud-sdk}/bin/gcloud storage rsync "/tank/immich_enc" "gs://nequi-nas-i/" --recursive --delete-unmatched-destination-objects''
       ];
     };
 

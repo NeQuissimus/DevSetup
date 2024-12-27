e522b330c59984c3bc3aae0a3829324f52ddcb37
Tim Steinbach <tim.steinbach@shopify.com>
2024-12-27 14:24:37 -0500

    Docker -> modules

--- c220.nix
+++ c220.nix
@@ -10,18 +10,8 @@ let
       "itzg/minecraft-server:java17@sha256:3a50610894211a5bc7075b853372f8803065067a41d7965b177b297f5bb543a8";
     musicAssistant =
       "ghcr.io/music-assistant/server:2.3.4@sha256:301cc44d2405e1f12953a44ddb65454630007755af9ada9d204bc1a3b9a06175";
-    ollama =
-      "ollama/ollama:0.5.4@sha256:18bfb1d605604fd53dcad20d0556df4c781e560ebebcd923454d627c994a0e37";
-    openWebui =
-      "ghcr.io/open-webui/open-webui:0.5.1@ sha256:a6f2a27e058bbb7b1278aae30b76a83e86d5fb118911582ac52e327714a76e46 ";
-    openwakeword =
-      "rhasspy/wyoming-openwakeword:1.10.0@sha256:3165a5cd8aef84beb882e640aa1f5c01c97f8c0b1f50016164ecdf2ab65d033a";
     pihole =
       "pihole/pihole:2024.07.0@sha256:0def896a596e8d45780b6359dbf82fc8c75ef05b97e095452e67a0a4ccc95377";
-    piper =
-      "rhasspy/wyoming-piper:1.5.0@sha256:b6bf208855f26168790ed336ad16576b2fb290f31b51fb98aca496a45561516f";
-    whisper =
-      "rhasspy/wyoming-whisper:2.4.0@sha256:2687f79715734606f856fc4478351ce91a76bdaf6899086bcb837eb9ac7cddb8";
   };
 in {
   imports = [
@@ -71,16 +61,17 @@ in {
     firewall = {
       allowPing = true;
       allowedTCPPorts = [
-        80
-        1400
-        3000
-        8095
-        8097
-        8123
-        config.services.minecraft-server.serverProperties.server-port
+        53 # Pihole
+        80 # Pihole
+        1400 # Home Assistant
+        5580 # Matter
+        8095 # Music Assistant
+        8096 # Music Assistant
+        8097 # Music Assistant
+        8123 # Home Assistant
+        23565 # Minecraft
       ];
-      allowedUDPPorts =
-        [ config.services.minecraft-server.serverProperties.server-port ];
+      allowedUDPPorts = [ 53 ];
       enable = true;
     };
 
@@ -113,45 +104,11 @@ in {
 
     cron = {
       enable = true;
-      systemCronJobs = [
-        "27 1 * * 0 root reboot"
-        ''
-          45 1 * * 0 nequi ${pkgs.curl}/bin/curl http://localhost:11434/api/chat -d '{"model": "qwen2.5:3b", "keep_alive": -1}' ''
-      ];
+      systemCronJobs = [ "27 1 * * 0 root reboot" ];
     };
 
     logrotate.checkConfig =
       false; # https://github.com/NixOS/nixpkgs/pull/237414
-
-    minecraft-server = {
-      dataDir = "/var/lib/minecraft";
-      declarative = true;
-      enable = false;
-      eula = true;
-
-      jvmOpts =
-        "-Xms2G -Xmx8G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:-UseBiasedLocking -XX:UseAVX=3 -XX:+UseStringDeduplication -XX:+UseFastUnorderedTimeStamps -XX:+UseAES -XX:+UseAESIntrinsics -XX:UseSSE=4 -XX:+UseFMA -XX:AllocatePrefetchStyle=1 -XX:+UseLoopPredicate -XX:+RangeCheckElimination -XX:+EliminateLocks -XX:+DoEscapeAnalysis -XX:+UseCodeCacheFlushing -XX:+SegmentedCodeCache -XX:+UseFastJNIAccessors -XX:+OptimizeStringConcat -XX:+UseCompressedOops -XX:+UseThreadPriorities -XX:+OmitStackTraceInFastThrow -XX:+TrustFinalNonStaticFields -XX:ThreadPriorityPolicy=1 -XX:+UseInlineCaches -XX:+RewriteBytecodes -XX:+RewriteFrequentPairs -XX:+UseNUMA -XX:-DontCompileHugeMethods -XX:+UseFPUForSpilling -XX:+UseFastStosb -XX:+UseNewLongLShift -XX:+UseVectorCmov -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseXmmLoadAndClearUpper -XX:+UseXmmRegToRegMoveAll -Dfile.encoding=UTF-8 -Xlog:async -Djava.security.egd=file:/dev/urandom";
-
-      package = pkgs.minecraftServers.vanilla-1-19;
-
-      serverProperties = {
-        allow-flight = true;
-        difficulty = 2;
-        enforce-whitelist = true;
-        level-seed = "5743982194324514800";
-        max-players = 5;
-        max-tick-time = 120000;
-        max-world-size = 15000;
-        motd = "NeQuissimus MC";
-        pvp = false;
-        server-port = 25565;
-        snooper-enabled = false;
-        spawn-protection = 0;
-        white-list = true;
-      };
-
-      whitelist = { germadian = "582bf397-4392-48b5-b7c1-7511aa15d51e"; };
-    };
 
     ntp = {
       enable = true;
@@ -163,11 +120,72 @@ in {
       ];
     };
 
+    ollama = {
+      enable = true;
+
+      environmentVariables = {
+        OLLAMA_MAX_LOADED_MODELS = "1";
+        OLLAMA_MAX_QUEUE = "2";
+        OLLAMA_NUM_PARALLEL = "1";
+      };
+
+      home = "/var/lib/ollama";
+      host = "0.0.0.0";
+
+      loadModels = [ "qwen2.5:3b" ];
+
+      openFirewall = true;
+    };
+
+    open-webui = {
+      enable = true;
+
+      environment = {
+        ENABLE_COMMUNITY_SHARING = "False";
+        ENABLE_MESSAGE_RATING = "False";
+        ENABLE_MODEL_FILTER = "True";
+        ENABLE_OPENAI_API = "False";
+        ENABLE_SIGNUP = "False";
+        MODEL_FILTER_LIST = "mistral:7b;qwen2.5:3b";
+        OLLAMA_API_BASE_URL =
+          "http://127.0.0.1:${toString config.services.ollama.port}";
+        SAFE_MODE = "True";
+        WEBUI_AUTH = "False";
+      };
+
+      host = "0.0.0.0";
+      openFirewall = true;
+      port = 3000;
+    };
+
     openssh.enable = true;
 
     thermald.enable = true;
 
     upower.enable = true;
+
+    wyoming = {
+      faster-whisper.servers."kevin" = {
+        enable = true;
+        language = "en";
+        model = "tiny-int8";
+        uri = "tcp://0.0.0.0:10300";
+      };
+
+      openwakeword = {
+        customModelsDirectories = [ "/var/lib/openwakeword" ];
+
+        enable = true;
+
+        preloadModels = [ "ok_kevin" ];
+      };
+
+      piper.servers."kevin" = {
+        enable = true;
+        uri = "tcp://0.0.0.0:10200";
+        voice = "en_GB-northern_english_male-medium";
+      };
+    };
 
     zfs.autoScrub = {
       enable = true;
@@ -182,9 +200,6 @@ in {
       "d /var/lib/mc 0755 nequi docker"
       "d /var/lib/mc2 0755 nequi docker"
       "d /var/lib/musicassistant 0755 nequi docker"
-      "d /var/lib/ollama 0755 nequi docker"
-      "d /var/lib/openwakeword 0755 nequi docker"
-      "d /var/lib/open-webui 0755 nequi docker"
       "d /var/lib/pihole 0755 nequi docker"
       "L+ ${config.services.minecraft-server.dataDir}/ops.json - - - - /etc/minecraft/ops.json"
     ];
@@ -199,15 +214,7 @@ in {
       homeassistant = {
         autoStart = true;
 
-        dependsOn = [
-          "matter"
-          "musicassistant"
-          "ollama"
-          "openwakeword"
-          "pihole"
-          "piper"
-          "whisper"
-        ];
+        dependsOn = [ "matter" "musicassistant" "pihole" ];
 
         environment.TZ = "America/Toronto";
         extraOptions = [ "--network=host" ];
@@ -272,47 +279,6 @@ in {
         volumes = [ "/var/lib/musicassistant:/data" ];
       };
 
-      ollama = {
-        autoStart = true;
-        environment = {
-          OLLAMA_MAX_LOADED_MODELS = "1";
-          OLLAMA_MAX_QUEUE = "2";
-          OLLAMA_NUM_PARALLEL = "1";
-        };
-        image = dockerImages.ollama;
-        ports = [ "11434:11434" ];
-        volumes = [ "/var/lib/ollama:/root/.ollama" ];
-      };
-
-      open-webui = {
-        autoStart = true;
-        dependsOn = [ "ollama" ];
-        environment = {
-          ENABLE_COMMUNITY_SHARING = "False";
-          ENABLE_MESSAGE_RATING = "False";
-          ENABLE_MODEL_FILTER = "True";
-          ENABLE_OPENAI_API = "False";
-          ENABLE_SIGNUP = "False";
-          MODEL_FILTER_LIST = "mistral:7b;qwen2.5:3b";
-          PORT = "3000";
-          SAFE_MODE = "True";
-          WEBUI_AUTH = "False";
-        };
-
-        extraOptions =
-          [ "--network=host" "--add-host=host.docker.internal:host-gateway" ];
-        image = dockerImages.openWebui;
-        volumes = [ "/var/lib/open-webui:/app/backend/data" ];
-      };
-
-      openwakeword = {
-        autoStart = true;
-        cmd = [ "--custom-model-dir" "/custom" "--preload-model" "hey_jarvis" ];
-        image = dockerImages.openwakeword;
-        ports = [ "10400:10400" ];
-        volumes = [ "/var/lib/openwakeword:/custom" ];
-      };
-
       pihole = {
         autoStart = true;
         environment = {
@@ -325,20 +291,6 @@ in {
           "/var/lib/pihole/etc-pihole:/etc/pihole"
           "/var/lib/pihole/etc-dnsmasq.d:/etc/dnsmasq.d"
         ];
-      };
-
-      piper = {
-        autoStart = true;
-        cmd = [ "--voice" "en_GB-northern_english_male-medium" ];
-        image = dockerImages.piper;
-        ports = [ "10200:10200" ];
-      };
-
-      whisper = {
-        autoStart = true;
-        cmd = [ "--model" "tiny-int8" "--language" "en" ];
-        image = dockerImages.whisper;
-        ports = [ "10300:10300" ];
       };
     };
   };
